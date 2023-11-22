566d1147dd1ea066299c0a4b5f2c321d98e636e8
Bash-Clevin <clevinbash@gmail.com>
2023-11-22 19:39:34 +0300

    chore(test): use a testing table

--- http-unit-test/sum_test.go
+++ http-unit-test/sum_test.go
@@ -7,9 +7,22 @@ import (
 )
 
 func TestInts(t *testing.T) {
-	s := sum.Ints(1, 2, 3, 4, 5, 7)
 
-	if s != 15 {
-		t.Errorf("Expected a sum of 15 but got %d", s)
+	testingTable := []struct {
+		name    string
+		numbers []int
+		sum     int
+	}{
+		{name: "one to five", numbers: []int{1, 2, 3, 4, 5}, sum: 15},
+		{name: "nil", numbers: nil, sum: 0},
+		{name: "one and minus one", numbers: []int{1, -1}, sum: 0},
+	}
+
+	for _, testCase := range testingTable {
+		s := sum.Ints(testCase.numbers...)
+
+		if s != testCase.sum {
+			t.Errorf("Sum of %v should be %d but got %d", testCase.name, testCase.sum, s)
+		}
 	}
 }
