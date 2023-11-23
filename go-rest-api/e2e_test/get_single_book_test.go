651b9ff2e58197fd675929379f89729ce5ddaff0
Bash-Clevin <clevinbash@gmail.com>
2023-11-23 18:17:58 +0300

    feat: Add book endpoints + tests

---
+++ go-rest-api/e2e_test/get_single_book_test.go
@@ -0,0 +1,53 @@
+package e2etest_test
+
+import (
+	"database/sql"
+	"io"
+	"net/http"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/suite"
+)
+
+type GetSingleBookSuite struct {
+	suite.Suite
+}
+
+func TestGetSingleBookSuite(t *testing.T) {
+	suite.Run(t, new(GetSingleBookSuite))
+}
+
+func (s *GetSingleBookSuite) TestGetBookThatDoesNotExist() {
+	c := http.Client{}
+
+	r, _ := c.Get("http://localhost:8080/book/123456789")
+	body, _ := io.ReadAll(r.Body)
+
+	s.Equal(http.StatusNotFound, r.StatusCode)
+	s.JSONEq(`{"code": "001", "msg": "No Book with ISBN 123456789"}`, string(body))
+}
+
+func (s *GetSingleBookSuite) TestGetBookThatDoesExist() {
+	db, _ := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+
+	db.Exec("INSERT INTO book (isbn, name,  image, genre, year_published) VALUES ('987654321', 'Testing all stuff', 'testing.jpg', 'computing', 2021 )")
+
+	c := http.Client{}
+
+	r, _ := c.Get("http://localhost:8080/book/987654321")
+	body, _ := io.ReadAll(r.Body)
+
+	s.Equal(http.StatusOK, r.StatusCode)
+
+	expectedBody := `{
+		"isbn": "987654321",
+		"name": "Testing all stuff",
+		"image": "testing.jpg",
+		"genre": "computing",
+		"year_published": 2021
+	}`
+	s.JSONEq(expectedBody, string(body))
+
+}
--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/postgres"
+
+	_ "github.com/golang-migrate/migrate/source/file"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 type health struct {
@@ -13,7 +22,43 @@ type health struct {
 	Messages []string `json:"messages"`
 }
 
+type jsonError struct {
+	Code string `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+type Book struct {
+	ISBN          string `json:"isbn"`
+	Name          string `json:"name"`
+	Image         string `json:"image"`
+	Genre         string `json:"genre"`
+	YearPublished int    `json:"year_published"`
+}
+
 func main() {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error connecting to DB: %s", err.Error())
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+
+	if err != nil {
+		log.Fatalf("Error making DB driver: %s", err.Error())
+	}
+
+	migrator, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"postgres",
+		driver,
+	)
+
+	migrator.Steps(2)
+
+	if err != nil {
+		log.Fatalf("Error making migration engine: %s", err.Error())
+	}
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +72,43 @@ func main() {
 		w.Write(b)
 
 		w.WriteHeader(http.StatusOK)
+	})
+
+	r.HandleFunc("/book/{isbn}", func(w http.ResponseWriter, r *http.Request) {
+		v := mux.Vars(r)
+
+		isbn := v["isbn"]
+		e := jsonError{
+			Code: "001",
+			Msg:  fmt.Sprintf("No Book with ISBN %s", isbn),
+		}
+
+		b := Book{}
+		row := db.QueryRow("SELECT isbn, name, image, genre, year_published FROM book WHERE isbn = $1", isbn)
+		err := row.Scan(
+			&b.ISBN,
+			&b.Name,
+			&b.Image,
+			&b.Genre,
+			&b.YearPublished,
+		)
+
+		if err != nil {
+			body, _ := json.Marshal(e)
+
+			w.WriteHeader(http.StatusNotFound)
+
+			w.Write(body)
+
+			return
+		}
+
+		body, _ := json.Marshal(b)
+
+		w.WriteHeader(http.StatusOK)
+
+		w.Write(body)
+
 	})
 
 	s := http.Server{
