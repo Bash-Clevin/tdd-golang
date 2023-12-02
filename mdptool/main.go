0a02a58fecf6e9de839f05c5c3395739d14ec057
Bash-Clevin <clevinbash@gmail.com>
2023-12-02 12:51:55 +0300

    feat: Add integration tests

--- mdptool/main.go
+++ mdptool/main.go
@@ -12,19 +12,14 @@ import (
 )
 
 const (
-	header = `
-	<!DOCTYPE html>
-	<html>
-		<head>
-			<meta http-equiv="content-type" content="text/html; charset=utf-8">
-			<title>Markdown Preview Tool</title>
-		</head>
-		<body>
-	`
-	footer = `
-		</body>
-	​</html>
-	`
+	header = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Markdown Preview Tool</title>
+  </head>`
+	footer = `</body>  ​
+</html>`
 )
 
 func main() {
