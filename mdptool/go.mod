be6d979f969bce980d1d795bae0db941d6393658
Bash-Clevin <clevinbash@gmail.com>
2023-12-02 11:59:49 +0300

    feat: Sample markdown parser

---
+++ mdptool/go.mod
@@ -0,0 +1,15 @@
+module github.com/Bash-Clevin/tdd-golang/mdptool
+
+go 1.21.4
+
+require (
+	github.com/microcosm-cc/bluemonday v1.0.26
+	github.com/russross/blackfriday v1.6.0
+	github.com/russross/blackfriday/v2 v2.1.0
+)
+
+require (
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
---
+++ mdptool/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
+)
+
+const (
+	header = `
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta http-equiv="content-type" content="text/html; charset=utf-8">
+			<title>Markdown Preview Tool</title>
+		</head>
+		<body>
+	`
+	footer = `
+		</body>
+	​</html>
+	`
+)
+
+func main() {
+	fileName := flag.String("file", "", "Markdown file to preview")
+	flag.Parse()
+
+	// If user did not provide input file, show usage information
+	if *fileName == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err := run(*fileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(fileName string) error {
+	// Read all the data from the input file and check for errors
+	input, err := os.ReadFile(fileName)
+	if err != nil {
+		return err
+	}
+
+	htmlData := parseContent(input)
+
+	outName := fmt.Sprintf("%s.html", filepath.Base(fileName))
+	fmt.Println(outName)
+
+	return saveHTML(outName, htmlData)
+}
+
+func parseContent(input []byte) []byte {
+	// Parse the markdown file through blackfriday and bluemonday​
+	// to generate a valid and safe HTML
+
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
+
+	// Create a buffer of bytes to write to file
+	var buffer bytes.Buffer
+
+	// Write html to bytes buffer
+	buffer.WriteString(header)
+	buffer.Write(body)
+	buffer.WriteString(footer)
+
+	return buffer.Bytes()
+}
+
+func saveHTML(outFName string, data []byte) error {
+	// Write the bytes to the file
+	return os.WriteFile(outFName, data, 06444)
+}
